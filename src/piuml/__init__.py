52b4eafe516da732e70db4d1192d14971fbb587d
wrobell <wrobell@pld-linux.org>
2010-03-17 00:25:26 +0000

    - documentation updates

---
+++ src/piuml/__init__.py
@@ -0,0 +1,32 @@
+#
+# piUML - UML diagram generator.
+#
+# Copyright (C) 2010 by Artur Wroblewski <wrobell@pld-linux.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+"""
+piUML is UML diagram generator.
+
+piUML provides simple and powerful language, which allows to easily write
+complex diagrams in UML notation. Diagram information defined with piUML
+language is analyzed and diagram is rendered into PDF file using automatic
+layout.
+"""
+
+__version__ = '0.1.0'
+
+
+# vim: sw=4:et:ai
