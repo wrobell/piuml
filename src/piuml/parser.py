781fb4ab9a5c7a18c28a72c2c7602221c53ae41f
wrobell <wrobell@pld-linux.org>
2010-03-24 01:48:08 +0000

    - documentation cleanup

--- src/piuml/parser.py
+++ src/piuml/parser.py
@@ -1,3 +1,22 @@
+#
+# piUML - UML diagram generator.
+#
+# Copyright (C) 2010 by Artur Wroblewski <wrobell@pld-linux.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 from spark import GenericScanner, GenericParser, GenericASTTraversal
 
 import re
@@ -7,8 +26,7 @@ from collections import namedtuple
 
 class ParseError(Exception):
     """
-    Parsing exception. Raised when input data cannot be parse or when
-    created gallery data model is inconsistent.
+    piUML language parsing exception.
     """
     def __init__(self, msg):
         super(ParseError, self).__init__(msg)
