8d43f453affac4763d855826ef56db3e4622ed13
wrobell <wrobell@pld-linux.org>
2010-04-07 23:25:24 +0100

    - metapost: "dir" is a keyword

--- src/piuml/renderer/mp.py
+++ src/piuml/renderer/mp.py
@@ -50,6 +50,7 @@ def id2mp(id):
     id = id.replace('a', 'aa').replace('1', 'a')
     id = id.replace('b', 'bb').replace('2', 'b')
     id = id.replace('c', 'cc').replace('3', 'c')
+    id = id.replace('dir', 'dirdir')
     return id
 
 
