d04a7d46522edf4dcbcd54e55997b20a9f965fb3
wrobell <wrobell@pld-linux.org>
2012-01-24 22:22:57 +0000

    - s/2010/2010 - 2012/

--- src/piuml/renderer/text.py
+++ src/piuml/renderer/text.py
@@ -1,7 +1,7 @@
 #
 # piUML - UML diagram generator.
 #
-# Copyright (C) 2010 by Artur Wroblewski <wrobell@pld-linux.org>
+# Copyright (C) 2010 - 2012 by Artur Wroblewski <wrobell@pld-linux.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
